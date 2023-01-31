16f56ad9f2768ac016dfdeb590a6a7d859530f41
MarcosYH <marcosdeimos18@gmail.com>
2023-01-31 17:56:58 +0100

    components

---
+++ src/components/Banner.js
@@ -0,0 +1,8 @@
+
+function Banner() {
+    const title = "La maison jungle"
+    const soustitle= "bienvenu à vous"
+      return (<div> <h1>{title}</h1> <h2>{soustitle}</h2>  </div> )
+    }
+
+    export default Banner

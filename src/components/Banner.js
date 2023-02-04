b9b64e3ce5f3729f6a9125497c0009691115fb00
MarcosYH <marcosdeimos18@gmail.com>
2023-02-04 23:32:25 +0100

    logo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,14 @@
-
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
 function Banner() {
     const title = "La maison jungle"
     const soustitle= "bienvenu à vous"
-      return (<div> <h1>{title}</h1> <h2>{soustitle}</h2>  </div> )
+      return (
+        <div className='banner'> 
+        <img src={logo} alt='La maison jungle' className='logo'  />
+        <h1 className='lmj-title'>{title}</h1>
+        <h2>{soustitle}</h2>  
+        </div> )
     }
 
     export default Banner
